fcc6bd813e1f99dde34f70ebae5d7856806b0983
Zlaam <mrzalaam@gmail.com>
2023-11-30 03:41:20 +0500

    pakage.json modifed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import RadialGradient from "./Components/Background Animated/bgAnimated";
 import App from "./App";
 ReactDOM.createRoot(document.querySelector("#root")).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <RadialGradient>
-      <App />
-    </RadialGradient>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <RadialGradient>
+        <App />
+      </RadialGradient>
+    </BrowserRouter>
+  </React.StrictMode>
 );
