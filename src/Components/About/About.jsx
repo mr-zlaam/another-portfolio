40031ff58b6dcd4ccba3cdd9f711fc478effe8ef
Zlaam <mrzalaam@gmail.com>
2023-11-29 23:52:42 +0500

    Last stable data

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,66 +1,60 @@
-import {} from "react";
 import "./About.css";
 import { Link } from "react-router-dom";
 import { BsArrowDown } from "react-icons/bs";
-
+import Data from "../../aboutdata.json";
 const About = () => {
+  const { title, image, content } = Data;
+  const date = new Date().getDate();
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
   return (
     <>
       <section className="about">
-        <div className="resume">
-          <div className="downarrow">
-            <BsArrowDown size={20} />
+        <div>
+          <div className="resume">
+            <div className="downarrow">
+              <BsArrowDown size={20} />
+            </div>
+            <Link className="resume_btn">Resume</Link>
           </div>
-          <Link className="resume_btn">Resume</Link>
+          <div className="main_about">
+            <div className="info_about">
+              <h1>Who am i?</h1>
+              <p>
+                <h4>
+                  <strong>Hello there!</strong>
+                </h4>
+                I am known as Zlaam. I am a dedicated and skilled FullStack-web
+                App developer with a 1.5 Years of hands-on experience. My true
+                passion lies in transforming concepts into tangible digital
+                solutions, and I wholeheartedly embrace new challenges that come
+                my way. I am proficient in languages such as JavaScript and
+                Python, I specialize in creating dynamic and responsive web
+                applications. My goal is to continue pushing the boundaries of
+                innovation in the ever-evolving field of web development.{date}
+              </p>
+              <div className="current_time_main_container">
+                <div className="date_container">
+                  <span className="date">{date}-</span>
+                  <span className="month">{month}-</span>
+                  <span className="year">{year}</span>
+                </div>
+                <span className="avail_msg">Available For Work</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="main_about">
-          <div className="info_about">
-            <h1>Who am i?</h1>
-            <p>
-              <h4>
-                <strong>Hello there!</strong>
-              </h4>
-              I am professionally known as Zlaam. I am a dedicated and skilled
-              web and App developer with a year of hands-on experience in the
-              industry. My true passion lies in transforming concepts into
-              tangible digital solutions, and I wholeheartedly embrace new
-              challenges that come my way.
-            </p>
-          </div>
-          <div className="skill">
-            <p className="para">
-              My expertise spans across a spectrum of technologies including:-
-            </p>
-            <div className="skill_controller">
-              <div className="htmls  web_chart">
-                <p className="para_child">HTML</p>
-                <p className="para_child">85%</p>
-              </div>
-              <div className="css  web_chart">
-                <p className="para_child">CSS</p>
-                <p className="para_child">78%</p>
-              </div>
-              <div className="js  web_chart">
-                <p className="para_child">JS</p>
-                <p className="para_child">80%</p>
-              </div>
-              <div className="tailwind  web_chart">
-                <p className="para_child">Tailwind</p>
-                <p className="para_child">74%</p>
-              </div>
-              <div className="bootstrap  web_chart">
-                <p className="para_child">Bootsrap</p>
-                <p className="para_child">60%</p>
-              </div>
-              <div className="reactjs  web_chart">
-                <p className="para_child">React</p>
-                <p className="para_child">80%</p>
-              </div>
-              <div className="reactnative  web_chart">
-                <p className="para_child">React-Native</p>
-                <p className="para_child">70%</p>
-              </div>
-              <div className="zlaam"></div>
+        <div>
+          <div className="card">
+            python
+            <img src={image} alt="" />
+            <div className="card__content">
+              <p className="card__title">Card Title</p>
+              <p className="card__description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco.
+              </p>
             </div>
           </div>
         </div>
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import Header from "../Header/Header";
+import Header from "../Header/Header.jsx";
 import "./Portfolio.css";
 import Routess from "../Routes/Routes";
 const Portfolio = () => {
